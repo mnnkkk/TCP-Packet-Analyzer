e3e493734e59dbebafc66bbd1d84a3d3c937b7a1
mnnkkk <46259595+mnnkkk@users.noreply.github.com>
2021-07-13 15:51:06 +0800

    style: reformat

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -6,12 +6,13 @@ FILE_PATH = "p01.pcap"
 SENDER = "10.179.59.38"
 RECEIVER = "10.68.33.170"
 
+
 class Packet():
     def __init__(self, data):
         self.ethernet = dpkt.ethernet.Ethernet(data)
         self.ip = self.ethernet.data
         self.tcp = self.ip.data
-    
+
     def get_id(self):
         return (self.tcp.sport, get_ip(self.ip.src), self.tcp.dport, get_ip(self.ip.dst))
 
@@ -36,6 +37,7 @@ class Packet():
     def get_src(self):
         return get_ip(self.ip.src)
 
+
 class Flow():
     def __init__(self, sender, receiver):
         self.sender = sender
@@ -48,11 +50,10 @@ class Flow():
         # get window size scaling factor
         try:
             options = dpkt.tcp.parse_opts(self.handshake[0][-1].tcp.opts)
-            window_scale = [value for opt,value in options if opt == dpkt.tcp.TCP_OPT_WSCALE][0]
-            self.win_scaling = 2**int(window_scale.hex(),base=16)
+            window_scale = [value for opt, value in options if opt == dpkt.tcp.TCP_OPT_WSCALE][0]
+            self.win_scaling = 2 ** int(window_scale.hex(), base=16)
         except:
             a = 0
-        
 
     def __separate_handshake(self):
         # get the syn packet from sender
@@ -64,7 +65,7 @@ class Flow():
         # get the syn, ack packet from receiver who's ack is 1 more than seq of the sender's syn packet
         receiver_syn = None
         for packet in self.receiver:
-            if packet[-1].get_tcp_flags() == 0x12 and packet[-1].get_ack() == sender_syn[-1].get_seq()+1:
+            if packet[-1].get_tcp_flags() == 0x12 and packet[-1].get_ack() == sender_syn[-1].get_seq() + 1:
                 receiver_syn = packet
                 break
         # get the ack packet from sender who's ack is 1 more than seq of syn,ack packet
@@ -72,7 +73,7 @@ class Flow():
         for packet in self.sender:
             if packet[-1].get_tcp_flags() == 0x10:
                 try:
-                    if packet[-1].get_ack() == receiver_syn[-1].get_seq()+1:
+                    if packet[-1].get_ack() == receiver_syn[-1].get_seq() + 1:
                         sender_ack = packet
                         break
                 except:
@@ -80,11 +81,11 @@ class Flow():
         # get index in flow
         try:
             index_to_split = self.flow.index(sender_ack)
-            self.handshake = self.flow[:index_to_split+1]
+            self.handshake = self.flow[:index_to_split + 1]
             # check if the sender_ack is piggyback
-            if sender_ack[-1].get_payload_size() != 0: # sender_ack is piggyback
+            if sender_ack[-1].get_payload_size() != 0:  # sender_ack is piggyback
                 index_to_split -= 1
-            self.flow = self.flow[index_to_split+1:]
+            self.flow = self.flow[index_to_split + 1:]
         except:
             return
 
@@ -92,7 +93,8 @@ class Flow():
         return self.sender[0][-1].get_id()
 
     def get_transactions(self, start=0, end=2):
-        return [(packet[-1].get_seq(), packet[-1].get_ack(), packet[-1].get_window_size()*self.win_scaling) for packet in self.flow if packet[-1].get_src() == SENDER][start:end]
+        return [(packet[-1].get_seq(), packet[-1].get_ack(), packet[-1].get_window_size() * self.win_scaling) for packet
+                in self.flow if packet[-1].get_src() == SENDER][start:end]
 
     def get_throughput(self):
         # find the fin,ack from receiver
@@ -102,7 +104,7 @@ class Flow():
                 last_packet == packet
         # get index of last packet in flow
         index = self.flow.index(packet)
-        flow_in_period = self.flow[:index+1]
+        flow_in_period = self.flow[:index + 1]
         data_sent = sum([packet[-1].get_tcp_size() for packet in flow_in_period if packet[-1].get_src() == SENDER])
         period = flow_in_period[-1][1] - flow_in_period[0][1]
 
@@ -117,12 +119,12 @@ class Flow():
                 first_ack = packet
                 break
         start_time = self.flow[0][1]
-        RTT = round(first_ack[1]-start_time,ndigits=2)
+        RTT = round(first_ack[1] - start_time, ndigits=2)
         # get timestamps 
         timestamps = [packet[1] for packet in self.flow if packet[-1].get_src() == SENDER]
         # subtract start time from each timestamp
-        timestamps = [ts-start_time for ts in timestamps]
-        breakpoints = [i*RTT for i in range(1,num_of_sizes+1)]
+        timestamps = [ts - start_time for ts in timestamps]
+        breakpoints = [i * RTT for i in range(1, num_of_sizes + 1)]
         # get estimated window sizes
         win_sizes = []
         timestamp_counter = 0
@@ -136,11 +138,11 @@ class Flow():
                 timestamp_counter += 1
 
         return win_sizes
-    
+
     def get_retransmission(self):
         # find triple dup acks receive (using ack num)
         receive = Counter([packet[-1].get_ack() for packet in self.flow if packet[-1].get_src() == RECEIVER])
-        triple_dups_acks = [ack for ack,count in receive.items() if count > 3]
+        triple_dups_acks = [ack for ack, count in receive.items() if count > 3]
         # find duplicate acks sent (using seq num)
         sent = Counter([packet[-1].get_seq() for packet in self.flow if packet[-1].get_src() == SENDER])
         duplicate_seqs = [seq for seq, count in sent.items() if count > 1]
@@ -158,7 +160,7 @@ class Flow():
                 if count == 2:
                     first_dup_ack_count = packet[0]
                     break
-            
+
             # check that the retransmission is out of order
             count = 0
             for packet in self.sender:
@@ -169,17 +171,19 @@ class Flow():
                         out_of_order += 1
                     break
 
-        triple_dups_count = len(intersection)-out_of_order
-        timeout_transmissions = len(duplicate_seqs)-triple_dups_count
+        triple_dups_count = len(intersection) - out_of_order
+        timeout_transmissions = len(duplicate_seqs) - triple_dups_count
 
         return triple_dups_count, timeout_transmissions
+
 
 def get_ip(data):
     """
     Returns the IP addresss encode as 4 bytes in as . dot separated string.
     """
     data = data.hex()
-    return ".".join([str(int(data[i:i+2], base=16)) for i in range(0, len(data), 2)])
+    return ".".join([str(int(data[i:i + 2], base=16)) for i in range(0, len(data), 2)])
+
 
 def get_tcp_flows(file):
     flows = {}
@@ -196,7 +200,8 @@ def get_tcp_flows(file):
                 tcp = ip.data
                 src = get_ip(ip.src)
                 dst = get_ip(ip.dst)
-                if not (str(src) == SENDER and str(dst) ==RECEIVER) and not (str(dst) == SENDER and str(src) ==RECEIVER):
+                if not (str(src) == SENDER and str(dst) == RECEIVER) and not (
+                        str(dst) == SENDER and str(src) == RECEIVER):
                     continue
 
                 iden = (tcp.sport, src, tcp.dport, dst)
@@ -223,6 +228,7 @@ def get_tcp_flows(file):
 
     return actual_flows
 
+
 if __name__ == "__main__":
     with open(FILE_PATH, 'rb') as f:
         # process_pcap(f)
@@ -237,13 +243,16 @@ if __name__ == "__main__":
                 transactions = flow.get_transactions()
                 print("b) The first 2 transactions:")
                 for t_count, transaction in enumerate(transactions, start=1):
-                    print(f"Tranaction {t_count}: \n\tSequence number: {transaction[0]:,d}\n\tAck number: {transaction[1]:,d}\n\tReceive Window Size: {transaction[2]:,d}")
+                    print(
+                        f"Tranaction {t_count}: \n\tSequence number: {transaction[0]:,d}\n\tAck number: {transaction[1]:,d}\n\tReceive Window Size: {transaction[2]:,d}")
             except:
                 a = 0
             data_sent, period = flow.get_throughput()
-            print(f"c) Throughput: {data_sent/period:,f} bytes/second ({data_sent:,d} bytes sent in {period:.4f} seconds)")
+            print(
+                f"c) Throughput: {data_sent / period:,f} bytes/second ({data_sent:,d} bytes sent in {period:.4f} seconds)")
             print("PART B")
             print(f"1) The first 3 congestion window sizes: {flow.estimate_congestion_window_size()}")
             dup_ack_retransmission, timeout_retransmission = flow.get_retransmission()
-            print(f"2) \tRetransmission due to triple duplicate ack: {dup_ack_retransmission}\n\tRetransmission due to timeout: {timeout_retransmission}")
-            print("="*100)
+            print(
+                f"2) \tRetransmission due to triple duplicate ack: {dup_ack_retransmission}\n\tRetransmission due to timeout: {timeout_retransmission}")
+            print("=" * 100)
